dc63a2d14d890e566e436740c58e48b6d3659d6f
melek saidani <mbcmelek3@gmail.com>
2026-04-12 20:25:31 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,6 +15,12 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_a22x.mk \
 	$(LOCAL_DIR)/twrp_a22x.mk
+ 
 
-COMMON_LUNCH_CHOICES := twrp_a22x-eng
+COMMON_LUNCH_CHOICES := \
+    lineage_a22x-user \
+    lineage_a22x-userdebug \
+    lineage_a22x-eng \
+    twrp_a22x-eng \
