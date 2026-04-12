c9389db3e37902e28233187945a8ce2121055bbd
melek saidani <mbcmelek3@gmail.com>
2026-04-12 20:21:07 +0100

    Add files via upload

---
+++ lineage_a22x.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from a22x device
+$(call inherit-product, device/samsung/a22x/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a22x
+PRODUCT_NAME := lineage_a22x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := a22x
+PRODUCT_MANUFACTURER := samsung
