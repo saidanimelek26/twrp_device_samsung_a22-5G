61ce8f07a5ea96bb8383b8961d796bc8514d4bb6
melek saidani <mbcmelek3@gmail.com>
2026-04-13 23:55:32 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,7 +28,6 @@ TARGET_USES_UEFI := true
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a78
 
@@ -46,17 +45,15 @@ TARGET_SUPPORTS_64_BIT_APPS := true
 TARGET_BOARD_PLATFORM := mt6833
 TARGET_BOARD_PLATFORM_GPU := mali-g57
 
-
-# Kernel
-TARGET_PREBUILT_KERNEL := device/samsung/a22x/prebuilt/Image.gz
-TARGET_PREBUILT_DTB := device/samsung/a22x/prebuilt/dtb.img
-BOARD_PREBUILT_DTBOIMAGE := device/samsung/a22x/prebuilt/dtbo.img
-#BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+# Kernel (Prebuilt)
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 BOARD_INCLUDE_RECOVERY_DTBO := true
+
 TARGET_KERNEL_ARCH := arm64
 
-
-# Boot
+# Boot Image
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
@@ -66,6 +63,7 @@ BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_RAMDISK_OFFSET := 0x11088000
 BOARD_KERNEL_SECOND_OFFSET := 0xffffffffbff88000
 BOARD_KERNEL_TAGS_OFFSET := 0x07c08000
+
 BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
@@ -75,45 +73,41 @@ BOARD_MKBOOTIMG_ARGS += --board SRPTL28A004
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
 
+# ==================== RECOVERY CONFIG ====================
 
+# Critical flags to prevent "root" directory missing error
+TARGET_NO_BOOT := true                  # Skip normal boot ramdisk (important for recovery-only builds)
+BUILDING_RECOVERY_IMAGE := true
+BOARD_USES_RECOVERY_AS_BOOT := false
+TARGET_NO_RECOVERY := false
 
-
-
-# Recovery
+# Recovery flags
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 41943040
-
-
-
-
-# Lineage Recovery flags - MODIFIED
-TARGET_NO_RECOVERY := false
-BOARD_USES_RECOVERY_AS_BOOT := false
 BOARD_SUPPRESS_SECURE_ERASE := true
-
-BUILDING_RECOVERY_IMAGE := true
 BOARD_RECOVERY_RAMDISK_COMPRESSION := gzip
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 41943040
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/common
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_SCREEN_DENSITY := 420
 
-# Dynamic Partitions
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/common
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_RECOVERY_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/recovery
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 41943040
+
+# Dynamic Partitions (Super)
 BOARD_SUPER_PARTITION_SIZE := 7239368704
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 7239368704
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product odm
 
+# AVB (Android Verified Boot)
 BOARD_AVB_ENABLE := true
 BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
@@ -122,9 +116,7 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
-
-#sepolicy for fastdotd
-BOARD_SEPOLICY_DIRS += device/samsung/a22x/sepolicy/
-BOARD_RECOVERY_SEPOLICY_DIRS += device/samsung/a22x/sepolicy/recovery
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/
-BOARD_RECOVERY_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/recovery
+# Optional but recommended for MediaTek recovery builds
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_USES_RECOVERY_AS_BOOT := false
