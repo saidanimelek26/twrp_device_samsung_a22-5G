27c61506cab364cb39eb32b6bfe065e347f8a793
melek saidani <mbcmelek3@gmail.com>
2026-04-13 03:20:38 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,28 +1,13 @@
 #
 # Copyright (C) 2020 The Android Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 DEVICE_PATH := device/samsung/a22x
 
-# Bootloader
-BOARD_VENDOR := samsung
-TARGET_SOC := S96801AA1
-TARGET_BOOTLOADER_BOARD_NAME := SRPTL28A004
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-TARGET_USES_UEFI := true
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
 TARGET_ARCH := arm64
@@ -39,34 +24,29 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a55
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
-TARGET_USES_64_BIT_BINDER := true
-TARGET_SUPPORTS_64_BIT_APPS := true
-
-# Fix build issues
-ALLOW_MISSING_DEPENDENCIES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
-
-# APEX
-DEXPREOPT_GENERATE_APEX_IMAGE := true
-
-# File systems
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
+# Bootloader
+BOARD_VENDOR := samsung
+TARGET_SOC := S96801AA1
+TARGET_BOOTLOADER_BOARD_NAME := SRPTL28A004
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_USES_UEFI := true
 
 # Platform
 TARGET_BOARD_PLATFORM := mt6833
 TARGET_BOARD_PLATFORM_GPU := mali-g57
 
 # Kernel
-TARGET_PREBUILT_KERNEL := device/samsung/a22x/prebuilt/Image.gz
-TARGET_PREBUILT_DTB := device/samsung/a22x/prebuilt/dtb.img
-BOARD_PREBUILT_DTBOIMAGE := device/samsung/a22x/prebuilt/dtbo.img
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := false
 TARGET_KERNEL_ARCH := arm64
 
 # Boot
-BOARD_BOOT_HEADER_VERSION := 2
+BOARD_BOOTIMG_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz
@@ -80,31 +60,30 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-BOARD_MKBOOTIMG_ARGS += --board SRPTL28A004
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --board $(TARGET_BOOTLOADER_BOARD_NAME)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
-#BOARD_CUSTOM_BOOTIMG_MK := device/samsung/a22x/bootimg.mk
 
-# Recovery
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-
-# Lineage Recovery flags - MODIFIED
-TARGET_NO_RECOVERY := false
-BOARD_USES_RECOVERY_AS_BOOT := false
-BOARD_SUPPRESS_SECURE_ERASE := true
-
-BUILDING_RECOVERY_IMAGE := true
-BOARD_RECOVERY_RAMDISK_COMPRESSION := gzip
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+# File systems
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 41943040
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/common
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_SCREEN_DENSITY := 420
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# TWRP Configuration (simplified)
+TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true
+TW_NO_LEGACY_PROPS := true
+TARGET_USES_MKE2FS := true
+TW_NO_SCREEN_BLANK := true
 
 # Dynamic Partitions
 BOARD_SUPER_PARTITION_SIZE := 7239368704
@@ -112,51 +91,8 @@ BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 7239368704
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product odm
 
-# System as root
-BOARD_ROOT_EXTRA_FOLDERS := cache carrier data_mirror efs keyrefuge linkerconfig metadata omr optics prism spu
-
-# Workaround for error copying vendor files to recovery ramdisk
+# Workaround for vendor files
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 
-AB_OTA_UPDATER := false
-
-# Android Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
-
-# Battery
-TW_USE_LEGACY_BATTERY_SERVICES := true
-
-# Crypto
-TW_INCLUDE_CRYPTO := false
-TW_INCLUDE_CRYPTO_FBE := false
-TW_INCLUDE_FBE_METADATA_DECRYPT := false
-BOARD_USES_METADATA_PARTITION := true
-
-# Display
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
-TW_MAX_BRIGHTNESS := 306
-TW_DEFAULT_BRIGHTNESS := 200
-TW_Y_OFFSET := 50
-TW_H_OFFSET := -50
-
-# Recovery options
-TW_NO_LEGACY_PROPS := true
-TW_USE_TOOLBOX := true
-TARGET_USES_MKE2FS := true
-
-# Additional flags
-TW_NO_SCREEN_BLANK := true
-
-#sepolicy for fastdotd
-BOARD_SEPOLICY_DIRS += device/samsung/a22x/sepolicy/
-BOARD_RECOVERY_SEPOLICY_DIRS += device/samsung/a22x/sepolicy/recovery
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/
-BOARD_RECOVERY_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
